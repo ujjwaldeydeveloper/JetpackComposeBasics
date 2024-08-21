b8c6b98ba40c61da2e6698c7928738bacc203b62
Ujjwal <ujjwal.dey55@gmail.com>
2024-08-21 09:01:32 +0530

    new page added and shown based on state variable

--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +20,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,14 +32,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeBasicsTheme {
-                MyApp()
+                    MyApp()
+                }
             }
+        }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(
+                onContinueClicked = { shouldShowOnboarding = false }
+            )
+        } else {
+            Greetings()
         }
     }
 }
 
 @Composable
-private fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")) {
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
     Column(modifier = modifier.padding(16.dp)) {
         for (name in names) {
             Greeting(
@@ -48,14 +69,19 @@ private fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("W
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val isExpanded = remember { mutableStateOf(false) }
-    val extraPadding = if (isExpanded.value) 16.dp else 0.dp
+    var isExpanded by remember { mutableStateOf(false) }
+    val extraPadding = if (isExpanded) 16.dp else 0.dp
 
-    Surface(color = MaterialTheme.colorScheme.primary,
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier =  Modifier.weight(1f).padding(bottom = extraPadding)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(
                     text = "Hello",
                 )
@@ -64,12 +90,41 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 )
             }
             ElevatedButton(
-                onClick = { isExpanded.value = !isExpanded.value },
+                onClick = { isExpanded = !isExpanded },
             ) {
-                Text( if(isExpanded.value)"Show less"  else "Show more")
+                Text(if (isExpanded) "Show less" else "Show more")
             }
 
         }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JetpackComposeBasicsTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
@@ -78,5 +133,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     JetpackComposeBasicsTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun MyAppPreview() {
+    JetpackComposeBasicsTheme {
+        MyApp(Modifier.fillMaxSize())
     }
 }
