7b08f84f151607f83ab89ba488e0f1fbdc2d3f3e
Ujjwal <ujjwal.dey55@gmail.com>
2024-08-22 09:42:15 +0530

    formatted code

--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.jetpackcomposebasics
 
 import android.content.res.Configuration
 import android.os.Bundle
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.unit.dp
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,9 +9,13 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.Spring.DampingRatioHighBouncy
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
@@ -26,20 +28,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,9 +57,7 @@ fun MyApp(modifier: Modifier = Modifier) {
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     Surface(modifier) {
         if (shouldShowOnboarding) {
-            OnboardingScreen(
-                onContinueClicked = { shouldShowOnboarding = false }
-            )
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
         } else {
             Greetings()
         }
@@ -91,16 +88,14 @@ fun OnboardingScreen(
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
-    JetpackComposeBasicsTheme {
-        OnboardingScreen(onContinueClicked = {})
-    }
+    JetpackComposeBasicsTheme { OnboardingScreen(onContinueClicked = {}) }
 }
 
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = List (100) { "Android #$it" }
-//    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(100) { "Android #$it" }
+    //    names: List<String> = listOf("World", "Compose")
 ) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
         items( items = names) { name ->
@@ -128,14 +123,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun CardContend(name: String) {
     var isExpanded by rememberSaveable { mutableStateOf(false) }
 
-    Row(modifier = Modifier
-        .padding(24.dp)
-        .animateContentSize(
-            animationSpec = spring(
-                dampingRatio = DampingRatioHighBouncy,
-                stiffness = Spring.StiffnessLow
+    Row(
+        modifier =
+        Modifier.padding(24.dp)
+            .animateContentSize(
+                animationSpec =
+                spring(
+                    dampingRatio = DampingRatioHighBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
             )
-        )) {
+        ) {
         Column(
             modifier = Modifier
                 .weight(1f)
@@ -157,40 +155,34 @@ fun CardContend(name: String) {
                             "padding theme elit, sed do bouncy. ").repeat(4),
                 )
             }
-
         }
-        IconButton(onClick = { isExpanded = !isExpanded}) {
+        IconButton(onClick = { isExpanded = !isExpanded }) {
             Icon(
                 imageVector = if (isExpanded) Filled.ExpandLess else Filled.ExpandMore,
                 contentDescription = if (isExpanded) {
                     stringResource(R.string.show_less)
                 } else {
-                        stringResource(R.string.show_more)
+                    stringResource(R.string.show_more)
                 }
             )
-
-
         }
-
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetpackComposeBasicsTheme {
-        Greeting("Android")
-    }
+    JetpackComposeBasicsTheme { Greeting("Android") }
 }
 
-@Preview(showBackground = true,
+@Preview(
+    showBackground = true,
     widthDp = 320,
     uiMode = Configuration.UI_MODE_NIGHT_YES,
-    name = "Dark Mode")
+    name = "Dark Mode"
+)
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun MyAppPreview() {
-    JetpackComposeBasicsTheme {
-        MyApp(Modifier.fillMaxSize())
-    }
+    JetpackComposeBasicsTheme { MyApp(Modifier.fillMaxSize()) }
 }
